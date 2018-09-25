62953055e92e01792f83ebf9b2d4e9fde6e3adc9
cdriesler <cdriesler.iv@gmail.com>
2018-09-24 22:23:44 -0400

    implement dispatcher firestore watcher and write-to-file loop

--- Cicero.Dispatch/Program.cs
+++ Cicero.Dispatch/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Cloud.Firestore;
 
 namespace Cicero.Dispatch
 {
@@ -11,11 +12,50 @@ namespace Cicero.Dispatch
     {
         static void Main(string[] args)
         {
+            FirestoreDb db = FirestoreDb.Create("cicero-box");
+            CollectionReference queue = db.Collection("queue");
+
             while (true)
             {
-                Console.WriteLine("Loop!");
+                Console.WriteLine($"Loop at {DateTime.Now.ToString()}.");
 
-                Thread.Sleep(1500);
+                var docs = queue.GetSnapshotAsync();
+                docs.Wait();
+
+                var numDocs = docs.Result.Count;
+
+                Console.WriteLine($"{numDocs.ToString()} documents in queue.");
+
+                if (numDocs == 0) goto End;
+
+                var id = docs.Result[0].Id;
+                var data = docs.Result[0].GetValue<string>("payload");
+
+                Console.WriteLine(data);
+
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\cicero\\";
+                System.IO.File.WriteAllText(path + "current.dat", data);
+
+                var rhinoIsDone = false;
+
+                var res = queue.Document(id).DeleteAsync();
+                res.Wait();
+
+                while (!rhinoIsDone)
+                {
+                    if (System.IO.File.Exists(path + "svg\\" + id + ".svg"))
+                    {
+                        rhinoIsDone = true;
+                    }
+
+                    Console.WriteLine("Rhino is computing...");
+                    Thread.Sleep(1000);
+                }
+
+                //Dispatch svg to database.
+
+                End:
+                Thread.Sleep(1000);
             }
         }
     }
