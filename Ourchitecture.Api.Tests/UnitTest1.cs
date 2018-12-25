fa4b04e5a569ba6c75745c39dee50a13ceb4e3d6
cdriesler <cdriesler.iv@gmail.com>
2018-12-25 15:36:15 -0500

    pivot to nancy csharp api

---
+++ Ourchitecture.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ourchitecture.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
